7f9f0fb8f2c393c79f8ee645a98e90c64a178255
Gustav Madslund <gum@netcompany.com>
2021-02-14 21:24:45 +0100

    Add Go intro updating variables.

--- src/variables/variables.go
+++ src/variables/variables.go
@@ -62,4 +62,13 @@ func main() {
 	cupsOfCoffeeConsumed = 12
 	fmt.Println(cupsOfCoffeeConsumed)
 	
+	//Updating Variables
+	coolSneakers := 65.99
+	niceNecklace := 45.50
+	var taxCalculation float64
+	taxCalculation += coolSneakers
+	taxCalculation += niceNecklace
+	taxCalculation *= .08875
+	fmt.Println("Purchase of", coolSneakers + niceNecklace, "with 8.875% sales tax", taxCalculation, "equal to", coolSneakers + niceNecklace + taxCalculation)
+
 }
