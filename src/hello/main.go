c23d2f876ad3eefeb16262c27c849ed1865f3441
Gustav Madslund <gum@netcompany.com>
2021-02-14 15:19:37 +0100

    Add Go comments.

--- src/hello/main.go
+++ src/hello/main.go
@@ -6,4 +6,10 @@ import t "time"
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println(t.Now())
+	
+	//Comments
+	//Are we racing or coding?
+	/*fmt.Println("Ready")
+	fmt.Println("Set")*/
+	fmt.Println("Gooooo!")
 }
