14af23830eb9a712aa13c993d60fd087961c4b0f
Gustav Madslund <gum@netcompany.com>
2021-02-14 15:31:45 +0100

    Add the gopher project.

---
+++ src/gopher/gopher.go
@@ -0,0 +1,26 @@
+package main
+
+import f "fmt"
+
+func main() {
+	f.Println("  __      _")
+	f.Println("o'')}____//")
+	f.Println(" `_/      )")
+	f.Println(" (_(_/-(_/")
+	
+	f.Println()
+	f.Println()
+	
+	f.Println("    `.-::::::-.`    ")
+	f.Println(".:-::::::::::::::-:.")
+	f.Println("`_:::    ::    :::_`")
+	f.Println(" .:( ^   :: ^   ):. ")
+	f.Println(" `:::   (..)   :::. ")
+	f.Println(" `:::::::UU:::::::` ")
+	f.Println(" .::::::::::::::::. ")
+	f.Println(" O::::::::::::::::O ")
+	f.Println(" -::::::::::::::::- ")
+	f.Println(" `::::::::::::::::` ")
+	f.Println("  .::::::::::::::.  ")
+	f.Println("    oO:::::::Oo     ")
+}
