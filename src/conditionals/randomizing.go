10827e5a4d79b15b2eab85c269d5f0515fcbccec
Gustav Madslund <gum@netcompany.com>
2021-02-19 22:22:31 +0100

    Add scoped short declaration and radomizing.

---
+++ src/conditionals/randomizing.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	amountLeft := rand.Intn(10000)
+	
+	fmt.Println("amountLeft is: ", amountLeft)
+	
+	if amountLeft > 5000 {
+		fmt.Println("What should I speed this on?")
+	} else {
+		fmt.Println("Where did all my money go?")
+	}
+}
